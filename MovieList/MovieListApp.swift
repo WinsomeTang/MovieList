994f303e513df2489f96ac7f2ea414c82e3958df
Winsome Tang <tangwins@sheridancollege.ca>
2024-02-11 13:00:51 -0500

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Winsome Tang on 2024-02-11.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
